684bc315d9dfc2959ec590ce6cbaae01225b9a5f
arthur <d3cryp@gmail.com>
2022-05-23 19:11:07 -0300

    subii coisas novas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizComponent } from './pages/quiz/quiz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: QuizComponent},
+  {path:'**', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
