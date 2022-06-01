b51f3e23bc13b2ed0be341506d4fe169aa97c144
arthur <d3cryp@gmail.com>
2022-05-31 22:08:59 -0300

    Logica do login + Acerto BG

--- src/app/pages/quiz/quiz.component.ts
+++ src/app/pages/quiz/quiz.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
+  nickname: string = "";
+  acessKey: string = "157";
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  login(campoValue: string) {
+    this.nickname = campoValue;
+    // console.log(this.nickname, this.acessKey);
+
+    //logica redirecionamento dos guri
+    if (this.nickname == "" || this.nickname == undefined || this.nickname == null) {
+      console.log("algo de errado n esta certo");
+    }
+    else if (this.nickname == this.acessKey) {
+      console.log("entrou autor");
+    }
+    else if (this.nickname != null) {
+      console.log("entrou jogador");
+    }
+
+    
+  }
 }
