e47de71f1c5f8b2c74b8a8e66d66c60fccfaa78c
Henriquehf <94650299+HenriqueFoletto@users.noreply.github.com>
2022-06-11 22:11:07 -0300

    listaPerguntas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PerguntasComponent } from './pages/perguntas/perguntas.component';
 
 const routes: Routes = [
-  { path: '', component: PerguntasComponent}
+  { path: '', component: PerguntasComponent},
 ];
 
 @NgModule({
--- src/app/pages/perguntas/perguntas.component.ts
+++ src/app/pages/perguntas/perguntas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 
 @Component({
   selector: 'app-perguntas',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerguntasComponent implements OnInit {
 
-  constructor() { }
+  
+
+  constructor() {} 
 
   ngOnInit(): void {
+  
   }
 
+    perguntas = [
+   
+      {
+        id: 1,
+        pergunta: 'Pergunta-1',
+        data: '05/06/2022', 
+        enunciado:'abc',
+        alternativa: '1,2,3,4,5',
+        alternativacorreta: '2'
+      },
+      {
+        id: 2,
+        pergunta: 'Pergunta-2',
+        data: '06/06/2022', 
+        enunciado:'abd',
+        alternativa: '1,2,3,4,5',
+        alternativacorreta: '5'
+
+      },
+      {
+        id: 3,
+        pergunta: 'Pergunta-3',
+        data: '07/06/2022',  
+        enunciado:'abf',
+        alternativa: '1,2,3,4,5',
+        alternativacorreta: '1'
+
+      },
+      {
+        id: 4,
+        pergunta: 'Pergunta-4',
+        data: '08/06/2022',  
+        enunciado:'abg',
+        alternativa: '1,2,3,4,5',
+        alternativacorreta: '3'
+      }
+    ];   
 }
+
+
+
