6a3e02fd9fbd43663211dfe38cf9588d48b84bab
Henriquehf <94650299+HenriqueFoletto@users.noreply.github.com>
2022-06-14 13:52:39 -0300

    excluir concluido

--- src/app/pages/perguntas/perguntas.component.ts
+++ src/app/pages/perguntas/perguntas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//import { Perguntas } from './perguntas';
+
 
 @Component({
   selector: 'app-perguntas',
@@ -7,64 +9,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerguntasComponent implements OnInit {
 
-  pergunta: string = '';
-  data: string = '';
-
+  
   constructor() {
     console.log(this.perguntas)
   } 
 
+  public perguntas = [
+    {
+      id: 1,
+      pergunta: 'Pergunta-1',
+      data: '05/06/2022', 
+      enunciado:'1+1',
+      alternativas: ['1','2','3','4','5'],
+      alternativacorreta: 2
+    },
+    {
+      id: 2,
+      pergunta: 'Pergunta-2',
+      data: '06/06/2022', 
+      enunciado:'2+3',
+      alternativas: ['1','2','3','4','5'],
+      alternativacorreta: 5
+
+    },
+    {
+      id: 3,
+      pergunta: 'Pergunta-3',
+      data: '07/06/2022',  
+      enunciado:'1+0',
+      alternativas: ['1','2','3','4','5'],
+      alternativacorreta: 1
+
+    },
+    {
+      id: 4,
+      pergunta: 'Pergunta-4',
+      data: '08/06/2022',  
+      enunciado:'1+2',
+      alternativas: ['1','2','3','4','5'],
+      alternativacorreta: 3
+    }
+  ];
+
   ngOnInit(): void {
   }
 
-  perguntas: any= [
-      {
-        id: 1,
-        pergunta: this.pergunta = 'Pergunta-1',
-        data: this.data = '05/06/2022', 
-        enunciado:'1+1',
-        alternativa: ['1','2','3','4','5'],
-        alternativacorreta: 2
-      },
-      {
-        id: 2,
-        pergunta: this.pergunta = 'Pergunta-2',
-        data: this.data = '06/06/2022', 
-        enunciado:'2+3',
-        alternativa: ['1','2','3','4','5'],
-        alternativacorreta: 5
-
-      },
-      {
-        id: 3,
-        pergunta: this.pergunta = 'Pergunta-3',
-        data: this.data = '07/06/2022',  
-        enunciado:'1+0',
-        alternativa: ['1','2','3','4','5'],
-        alternativacorreta: 1
-
-      },
-      {
-        id: 4,
-        pergunta: this.pergunta = 'Pergunta-4',
-        data: this.data = '08/06/2022',  
-        enunciado:'1+2',
-        alternativa: ['1','2','3','4','5'],
-        alternativacorreta: 3
-      }
-    ];
-
     criar(){
-
+    
     }
 
     editar(){
-      
+  
     }
 
     excluir(id: number){
-      console.log(`pergunta ${id} excluida`)
+      let pergunta = this.perguntas.findIndex(perguntas => perguntas.id === id)
+      if(pergunta !== -1){
+        this.perguntas.splice(pergunta, 1)
+        console.log(`pergunta ${id} excluida`)
       }
+    }
 }
+
 
 
