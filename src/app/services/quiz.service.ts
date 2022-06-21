315acf49a9a4b2d5c512d360a3740a3c2a315e67
brunocneuhaus <94646924+brunocneuhaus@users.noreply.github.com>
2022-06-20 22:12:37 -0300

    Get e Post EndPoint

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() {  }
+  constructor(private http: HttpClient) {} 
 
- 
+    getPerguntas() {
+      return this.http.get("caminhodobanco");
+    }
 
 }
