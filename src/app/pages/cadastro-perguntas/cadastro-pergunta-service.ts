1c92dda02ec73f58b4708951d63956900a6f6997
Gu1lh3rm3Fr4nca <guilherme.t.f@hotmail.com>
2022-06-27 22:06:19 -0300

    implementado parametro para edicao de pergunta

--- src/app/pages/cadastro-perguntas/cadastro-pergunta-service.ts
+++ src/app/pages/cadastro-perguntas/cadastro-pergunta-service.ts
@@ -4,9 +4,13 @@ import { HttpClient, } from '@angular/common/http';
 @Injectable({ providedIn: 'root' })
 export class CadastroPerguntaService {
 
+    editarPergunta(pergObject: Object[]) {
+      this.http.put('editar', pergObject);
+    }
+
     constructor(private http: HttpClient) { }
 
     cadastroPergunta(cadPerg: Object) {
-        return this.http.post('cadastro/salvaPergunta', cadPerg);
+      return this.http.post('cadastro/salvaPergunta', cadPerg);
     }
 }
--- src/app/pages/cadastro-perguntas/cadastro-perguntas.component.ts
+++ src/app/pages/cadastro-perguntas/cadastro-perguntas.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CadastroPerguntaService } from './cadastro-pergunta-service';
 
 
@@ -8,9 +8,10 @@ import { CadastroPerguntaService } from './cadastro-pergunta-service';
   templateUrl: './cadastro-perguntas.component.html',
   styleUrls: ['./cadastro-perguntas.component.css']
 })
-export class CadastroPerguntasComponent {
+export class CadastroPerguntasComponent implements OnInit {
 
   // variaveis de objeto da pergunta
+  id?: number;
   enunciadoP: string = "";
   resp1: string = "";
   resp2: string = "";
@@ -19,22 +20,49 @@ export class CadastroPerguntasComponent {
   resp5: string = "";
   respCerta: string = "";
   pergObject: Object[] = [];
+  
+  constructor(private service: CadastroPerguntaService, 
+              private route: ActivatedRoute) { }
 
-  constructor(private service: CadastroPerguntaService) { }
+  ngOnInit() {
+    this.pergObject = this.route.snapshot.queryParams["editar"];
+    this.pergObject.forEach(pergunta =>{
+      // this.enunciadoP = pergunta.enunciado,
+      // this.
+      // this.respCerta = pergunta.alternativacorreta
 
-  salvar() {
-    this.pergObject.push(
-      {
-        enunciadoP: this.enunciadoP, 
-        resp1: this.resp1,
-        resp2: this.resp2,
-        resp3: this.resp3,
-        resp4: this.resp4,
-        resp5: this.resp5,
-        respCerta: this.respCerta
-      }
-    );
-    this.service.cadastroPergunta(this.pergObject);
+    })
   }
 
+  salvar() {
+    if(this.id){
+      this.pergObject.push(
+        {
+          id: this.id,
+          enunciadoP: this.enunciadoP, 
+          resp1: this.resp1,
+          resp2: this.resp2,
+          resp3: this.resp3,
+          resp4: this.resp4,
+          resp5: this.resp5,
+          respCerta: this.respCerta
+        }
+      );
+    this.service.editarPergunta(this.pergObject);
+    }else{
+      this.pergObject.push(
+        {
+          enunciadoP: this.enunciadoP, 
+          resp1: this.resp1,
+          resp2: this.resp2,
+          resp3: this.resp3,
+          resp4: this.resp4,
+          resp5: this.resp5,
+          respCerta: this.respCerta
+        }
+      );
+      this.service.cadastroPergunta(this.pergObject);
+    }
+  }
 }
+
